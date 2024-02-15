4acab99b05c6989cd38b1d4dd0ee9586425c8fdb
Shutko Svitlana <svetalysenko25@gmail.com>
2024-02-15 10:49:02 +0200

    added homework 14 Glovo

--- src/main/java/com/company/glovo/service/order/jpa/OrderServiceImpl.java
+++ src/main/java/com/company/glovo/service/order/jpa/OrderServiceImpl.java
@@ -34,11 +34,13 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void update(Integer id, Order updatedOrderData) {
-        listOfOrders.stream().filter(result -> id.equals(result.getId())).forEach(result -> {
-            result.setDate(updatedOrderData.getDate());
-            result.setCost(updatedOrderData.getCost());
-            result.setProducts(updatedOrderData.getProducts());
-        });
+        listOfOrders.stream()
+                .filter(result -> id.equals(result.getId()))
+                .forEach(result -> {
+                    result.setDate(updatedOrderData.getDate());
+                    result.setCost(updatedOrderData.getCost());
+                    result.setProducts(updatedOrderData.getProducts());
+                });
     }
 
     @Override
